bb27b5e4fd6021b0735cd32781b495e0b9e6a052
RChant17 <st10481221@rcconnect.edu.za>
2025-05-26 15:40:48 +0200

    first commit

---
+++ test/MessageTest.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ST10481221 Rikhotso Engetelo Chant
+ */
+public class MessageTest {
+    
+}
